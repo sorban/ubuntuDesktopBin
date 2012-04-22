8c94fb57fc7ec046c77de179818b82b117700c6d
Stephen Orban <sorban@gmail.com>
2012-04-22 14:23:27 -0400

    adding mongodb install

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install make
 apt-get install vim
 
+#still need to setup ssh keys and set params for github
 apt-get install git-core git-gui git-doc
 
 #node
@@ -33,6 +34,7 @@ ec2-authorize default -p 22
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080
 iptables-save
 
+#still need to add deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen to sources.list, this won't work w/out it.
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 apt-get update
 apt-get install mongodb-10gen
